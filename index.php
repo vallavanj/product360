3b763d200ac99412916e0f983fbc81748787704a
Vallavan j <vallavan.j@ocsmain.com>
2018-12-19 14:57:19 +0530

    product 360 reel js

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html data-type="example" data-id="sequence-dual-orbit">
+  <head>
+    <title>Product 360</title>
+    <meta charset='utf-8' content='text/html' http-equiv='Content-type' />
+
+    <script src='http://code.jquery.com/jquery-1.9.1.min.js' type='text/javascript'></script>
+    <script src='jquery.reel.js' type='text/javascript'></script>
+
+
+    <style>
+      #green_marker {
+        width: 20px;
+        height: 20px;
+        margin-left: -20px;
+        background: green;
+      }
+	  #image-reel {
+				cursor: url(http://code.vostrel.net/jquery.reel.cur),move;
+		}
+    </style>
+  </head>
+  <body>
+
+    <img src="jewely.png" width="500" height="500"
+      class="reel"
+      id="image"
+      data-images="images/##.png"
+      data-footage="10"
+      data-cw="true"
+	  
+      data-orbital="3"
+     <!-- data-inversed="true"  -->
+	   <!-- data-speed="0.2"
+      >
+
+    
+
+  </body>
+</html>
